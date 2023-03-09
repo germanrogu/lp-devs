e4416412db700db06b83d54d3f8a62cbc0f97f93
germanrogu <german.rodriguezgu@gmail.com>
2023-03-09 12:01:04 -0500

    :construction: Input component

---
+++ src/components/ui/Inputs/InputText.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const InputText = () => {
+  return (
+    <div className="mb-3 pt-0">
+      <input
+        type="text"
+        placeholder="Placeholder"
+        className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+      />
+    </div>
+  );
+};
