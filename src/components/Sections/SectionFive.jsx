a1c474711801407035dae24cf819bfe7552107f4
germanrogu <german.rodriguezgu@gmail.com>
2023-03-09 17:54:27 -0500

    Refactor this code

--- src/components/Sections/SectionFive.jsx
+++ src/components/Sections/SectionFive.jsx
@@ -4,37 +4,26 @@ import {
   SignalFilled,
 } from "@ant-design/icons";
 import React from "react";
+import { ItemChart } from "../ui/ItemChart/ItemChart";
 
 export const SectionFive = () => {
   return (
-    <div className="flex flex-wrap py-6 bg-blue-600 h-160 md:h-60">
+    <div className="flex flex-wrap py-14 bg-blue-600">
       <div className="w-full md:w-full flex flex-col md:flex-row justify-center">
         <div className="w-full md:w-1/3">
-          <div className="flex flex-col justify-center ">
+          <ItemChart quantity={"2.620"}>
             <SignalFilled style={{ color: "#ffffff", fontSize: "80px" }} />
-            <p className="text-2xl font-bold text-white">2.620</p>
-            <p class="text-base  text-white">
-              Lorem ipsum dolor sit amet, consectur adipsigin elit.
-            </p>
-          </div>
+          </ItemChart>
         </div>
         <div className="w-full md:w-1/3">
-          <div className="flex flex-col justify-center ">
+          <ItemChart quantity={"132.620"}>
             <PieChartOutlined style={{ color: "#ffffff", fontSize: "80px" }} />
-            <p className="text-2xl font-bold text-white">2.620</p>
-            <p class="text-base text-white">
-              Lorem ipsum dolor sit amet, consectur adipsigin elit.
-            </p>
-          </div>
+          </ItemChart>
         </div>
         <div className="w-full md:w-1/3">
-          <div className="flex flex-col justify-center ">
+          <ItemChart quantity={"13.654"}>
             <BulbOutlined style={{ color: "#ffffff", fontSize: "80px" }} />
-            <p className="text-2xl font-bold text-white">2.620</p>
-            <p class="text-base text-white">
-              Lorem ipsum dolor sit amet, consectur adipsigin elit.
-            </p>
-          </div>
+          </ItemChart>
         </div>
       </div>
     </div>
