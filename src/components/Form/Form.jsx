3e539db677f6db6d9f1deefefe8efecede33a892
germanrogu <german.rodriguezgu@gmail.com>
2023-03-09 17:54:59 -0500

    Text color

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -38,7 +38,7 @@ export const Form = ({ onClick }) => {
               id="countries"
               className="relative block
             w-full px-3 py-2 border border-gray-300
-            placeholder-gray-500 text-gray-900
+            placeholder-gray-500 text-gray-500
             focus:outline-none focus:ring-indigo-500
             focus:border-indigo-500 focus:z-10 sm:text-sm"
             >
