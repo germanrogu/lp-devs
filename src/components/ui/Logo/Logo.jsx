9ed9bb882d4d342710cbe41de68a80023bdcaf85
germanrogu <german.rodriguezgu@gmail.com>
2023-03-09 16:08:36 -0500

    Logo conditional

--- src/components/ui/Logo/Logo.jsx
+++ src/components/ui/Logo/Logo.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import logo from "../../../assets/Logo.png";
+import logo_white from "../../../assets/Logo_White.png";
 
-export const Logo = () => {
+export const Logo = ({ isWhite = false }) => {
   return (
     <div className="flex flex-wrap justify-center">
-      <div className="w-20">
+      <div className={isWhite ? "w-28" : "w-20"}>
         <img
-          src={logo}
+          src={isWhite ? logo_white : logo}
           alt="image_logo"
-          className="rounded max-w-full h-auto align-middle border-none"
+          className="rounded max-w-full h-auto align-middle border-none "
         />
       </div>
     </div>
