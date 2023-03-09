795538be8147a6bdb350e635826c9291a03159ba
germanrogu <german.rodriguezgu@gmail.com>
2023-03-09 16:08:27 -0500

    Add other sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Footer } from "../components/Footer/Footer";
 import { Navbar } from "../components/Navbar/Navbar";
 import { SectionFive } from "../components/Sections/SectionFive";
 import { SectionFour } from "../components/Sections/SectionFour";
 import { SectionOne } from "../components/Sections/SectionOne";
+import { SectionSix } from "../components/Sections/SectionSix";
 import { SectionThree } from "../components/Sections/SectionThree";
 import { SectionTwo } from "../components/Sections/SectionTwo";
 
@@ -18,6 +20,8 @@ export const Home = () => {
       <SectionThree />
       <SectionFour />
       <SectionFive />
+      <SectionSix />
+      <Footer />
     </>
   );
 };
