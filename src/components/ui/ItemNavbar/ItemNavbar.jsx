2d33d3e8c27449860f6d70ecf56356fb377dbb73
germanrogu <german.rodriguezgu@gmail.com>
2023-03-09 17:54:36 -0500

    Add key

--- src/components/ui/ItemNavbar/ItemNavbar.jsx
+++ src/components/ui/ItemNavbar/ItemNavbar.jsx
@@ -4,8 +4,8 @@ import { itemsOptions } from "../../../constants/Items";
 export const ItemNavbar = () => {
   return (
     <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-      {itemsOptions.map((item) => (
-        <li className="nav-item">
+      {itemsOptions.map((item, index) => (
+        <li key={index} className="nav-item">
           <a
             className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75"
             href={`#${item}`}
