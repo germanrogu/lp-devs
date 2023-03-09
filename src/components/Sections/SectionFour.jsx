682844484ef0b91fa8d748e66a0396fc445d50e8
germanrogu <german.rodriguezgu@gmail.com>
2023-03-09 16:09:22 -0500

    layout of sections

--- src/components/Sections/SectionFour.jsx
+++ src/components/Sections/SectionFour.jsx
@@ -8,11 +8,9 @@ export const SectionFour = () => {
           LOREM IPSUM
         </h4>
         <p className="text-base font-normal leading-relaxed mt-0 mb-4 text-black-800">
-          I will be the leader of a company that ends up being worth billions of
-          dollars, because I got the answers. I understand culture. I am the
-          nucleus. I think that’s a responsibility that I have, to push
-          possibilities, to show people, this is the level that things could be
-          at.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Non quam
+          lacus suspendisse faucibus interdum posuere lorem ipsum dolor.
         </p>
       </div>
       <div className="w-full md:w-1/2 lg:w-1/2">
